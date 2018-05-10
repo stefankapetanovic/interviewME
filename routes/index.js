599a0b591d5597d289a2c5c805b69c9e85102b41
Matthew Turner <matthew.turner@utah.edu>
2018-05-09 20:59:25 -0600

    First pass at livestreaming with websockets

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,64 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const WebSocketServer = require('websocket').server;
+var http = require('http');
 
-let stream;
-let streamHasEnded = false;
+let connections = [];
 
 router.get('/', function(req, res, next) {
   res.render('static/index');
 });
 
 router.get('/interviewer', function(req, res, next) {
-   res.render('interviewer'); 
+   res.render('interviewer', {id:2}); 
 });
 
 router.get('/interviewee', function(req, res, next) {
-   res.render('interviewee'); 
+   res.render('interviewee', {id:1});
 });
 
-// TODO -- handle multiple streams; more elegantly handle chunks
-router.post('/startStream', function(req, res, next) {
-    if(fs.existsSync('./test.webm')){ fs.unlinkSync('./views/test.webm'); }
-    stream = fs.createWriteStream('./views/test.webm');
-    streamHasEnded = false;
-    res.sendStatus(200);
+const server = http.createServer(function(request, response) {
+    console.log((new Date()) + " Received request for " + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(6502, function() {
+    console.log((new Date()) + " Server is listening on port 6502");
+});
+const wsServer = new WebSocketServer({
+    httpServer: server,
+    maxReceivedFrameSize: 128000,
+    autoAcceptConnections: true // You should use false here! -- Review notes on how this should work in production
+});
+wsServer.on('connect', function(connection) {
+  console.log((new Date()) + " Connection accepted.");
+  connections.push(connection);
+  connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+          //console.log("Received Message: " + message.utf8Data);
+          // Process messages
+          let msg = JSON.parse(message.utf8Data);
+          connection.id = msg.id;
+          switch(msg.type) {
+            case 'streamingdata':
+                for (peer of connections) {
+                    if (peer.id !== connection.id) {
+                        peer.sendUTF(JSON.stringify(msg));
+                    }
+                }
+                return;
+          }
+      }
+  });
+  
+  // Handle the WebSocket "close" event; this means a user has logged off or has been disconnected.
+  connection.on('close', function(connection, closeReason, description) {
+    console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+    console.log(closeReason);
+    console.log(description);
+  });
 });
 
-router.post('/postChunk', function(req, res, next) {
-   req.on('readable', function() {
-      const payload = req.read();
-      if(payload && stream && !streamHasEnded) stream.write(payload);
-   });
-   res.sendStatus(200);
-});
-
-router.post('/lastChunk', function(req, res, next) {    
-    stream.end();
-    streamHasEnded = true;
-    res.sendStatus(200);
-});
-
-router.get('/interviewStream', function(req, res, next) {
-    const src = fs.createReadStream('./views/test.webm');
-    src.pipe(res);
-});
 
 module.exports = router;
